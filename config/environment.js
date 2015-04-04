55ea4fd7aee96e8d665b599309e9f16547f93b10
Justin Brown <justinbrown.work@gmail.com>
2015-04-03 22:19:01 -0400

    Use pods.

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'learning-liquid-fire',
+    podModulePrefix: 'learning-liquid-fire/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
